1fe45115ce859ca283cad9b94e2ac136b9aca656
Chris Rawles <crawles@google.com>
2024-05-20 14:14:22 -0700

    Internal change.
    PiperOrigin-RevId: 635567174

--- android_world/env/setup_device/setup_test.py
+++ android_world/env/setup_device/setup_test.py
@@ -12,17 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 from unittest import mock
 
 from absl.testing import absltest
 from android_env import env_interface
 from android_world.env import adb_utils
 from android_world.env import tools
-from android_world.env.setup_device import apps
 from android_world.env.setup_device import setup
 from android_world.utils import app_snapshot
-from android_world.utils import file_utils
 
 
 class SetupTest(absltest.TestCase):
@@ -32,25 +29,6 @@ class SetupTest(absltest.TestCase):
     self.mock_issue_generic_request = self.enter_context(
         mock.patch.object(adb_utils, 'issue_generic_request')
     )
-
-  @mock.patch.object(file_utils, 'tmp_file_from_cns')
-  @mock.patch.object(adb_utils, 'install_apk')
-  def test_install_apk(
-      self,
-      mock_install_apk,
-      mock_tmp_file_from_cns,
-  ):
-    env = mock.create_autospec(env_interface.AndroidEnvInterface)
-    apk = 'example.apk'
-
-    tmp_apk_file = '/tmp/example.apk'
-    mock_tmp_file_from_cns.return_value.__enter__.return_value = tmp_apk_file
-    setup._download_and_install_apk(apk, env)
-
-    mock_tmp_file_from_cns.assert_called_once_with(
-        os.path.join(apps.APP_DATA, apk),
-    )
-    mock_install_apk.assert_called_once_with(tmp_apk_file, env)
 
   @mock.patch.object(tools, 'AndroidToolController')
   @mock.patch.object(setup, '_download_and_install_apk')
