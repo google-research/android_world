9ec1b5e117964da664805f1926ec1555c8f71376
tlc4418 <tcoste99@gmail.com>
2024-11-27 11:29:51 +0000

    Remove the mandatory click before an input_text

--- android_world/env/actuation.py
+++ android_world/env/actuation.py
@@ -73,11 +73,12 @@ def execute_adb_action(
   elif action.action_type == 'input_text':
     text = action.text
     if text:
-      # First focus on enter text UI element.
-      click_action = copy.deepcopy(action)
-      click_action.action_type = 'click'
-      execute_adb_action(click_action, screen_elements, screen_size, env)
-      time.sleep(1.0)
+      # REMOVE THE MANDATORY CLICK BEFORE INPUT TEXT
+      # # First focus on enter text UI element.
+      # click_action = copy.deepcopy(action)
+      # click_action.action_type = 'click'
+      # execute_adb_action(click_action, screen_elements, screen_size, env)
+      # time.sleep(1.0)
       adb_utils.type_text(text, env, timeout_sec=10)
       adb_utils.press_enter_button(env)
     else:
