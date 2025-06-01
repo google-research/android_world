06079b36dd5e508ed63b042402f718eb2c84f3b3
celilyigit <celil.yigit@tum.de>
2025-06-01 13:31:51 +0200

    license renamed

--- random_minimal_runner.py
+++ random_minimal_runner.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright 2024 The android_world Authors.
+# Copyright 2025 Celil Yiğit & Sina Mohammad Rezaei
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
